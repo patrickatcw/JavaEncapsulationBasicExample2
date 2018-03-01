2e0781670078a887007bf0f8a4ee746f96e5e15c
patrickatcw <pj8392@yahoo.com>
2018-03-01 10:08:17 -0500

    initial program structure, directions for project and updated gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,21 @@
+package com.me;
+//directions
+/*
+- create a class and demonstrate proper encapsulation techniques
+- the class will be called printer, it will simulate a real computer printer
+- it should have fields for the toner level, number of pages printed,
+and also whether it's a duplex printer(capable of printing on both sides of the paper)
+- add methods to fill up the toner (up to a max. of 100%), and another
+method to simulate printing a page (which should increase the number
+of pages printed)
+- decide on the scope, whether to use constructors, and anything else you think
+is needed (remember scope is determining when the code is compiled, order)
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
